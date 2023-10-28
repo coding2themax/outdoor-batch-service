8cfe0b15728b0db9c4e0edda89ef5cecb95311fd
max quinones <maximilian.quinones@gmail.com>
2023-10-28 08:13:02 -0700

    init

---
+++ src/main/java/com/coding2themax/outdoor/outdoorbatchservice/OutdoorBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.outdoor.outdoorbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutdoorBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutdoorBatchServiceApplication.class, args);
+	}
+
+}
